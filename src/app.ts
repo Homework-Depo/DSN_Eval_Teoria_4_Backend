19a19eeb293555678b89279ae7b4bd898981bb2e
Javier Aponte <31341776+javier-aponte@users.noreply.github.com>
2023-06-29 23:06:02 -0500

    res.cookie

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost',
+  origin: 'http://3.94.173.130',
   credentials: true
 }));
 app.use(morgan("dev"));
